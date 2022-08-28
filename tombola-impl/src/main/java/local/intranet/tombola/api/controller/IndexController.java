fa0ad4dd6020d6624d61fd0a9da2a09fd4fcf499
Radek Kadner <radek.kadner@gmail.com>
2022-08-28 14:32:34 +0200

    .

--- tombola-impl/src/main/java/local/intranet/tombola/api/controller/IndexController.java
+++ tombola-impl/src/main/java/local/intranet/tombola/api/controller/IndexController.java
@@ -260,7 +260,6 @@ public class IndexController {
 	 * @return "index" for thymeleaf index.html {@link String}
 	 */
 	@GetMapping(value = { "/", "/page/{page}" }, produces = MediaType.TEXT_HTML_VALUE)
-	@PreAuthorize("hasRole('ROLE_userRole')")
 	public String getIndex(@PathVariable(value = "page", required = false) Integer pg, HttpServletRequest request,
 			Model model) {
 		AtomicInteger sumaCnt = new AtomicInteger();
